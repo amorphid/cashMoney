6baffdca9122f66c0f3462543ac0d78aa229909e
Michael Pope <mpope.cr@gmail.com>
2014-10-10 07:48:19 -0700

    Add test => X_X .shouldThrowError is a function

--- test/superDeadTest.js
+++ test/superDeadTest.js
@@ -5,3 +5,14 @@ X_X("superDead is a function", function () {
 X_X("X_X is equal to superDead", function () {
   return X_X === superDead;
 });
+
+X_X("X_X", function () {
+  X_X(".shouldThrowError", function () {
+    X_X("is a function", function () {
+      return typeof X_X.shouldThrowError === "function";
+    });
+
+    // X_X("returns true", function () {
+    // });
+  });
+});
