fd721a0f1a587eae694d72ddd30da761a00df49f
Michael Pope <mpope.cr@gmail.com>
2014-10-09 20:49:27 -0700

    Add test => testX_XequalsSuperDead

--- test/superDead_test.js
+++ test/superDead_test.js
@@ -2,4 +2,9 @@ function testSuperDeadIsAFunction () {
   return typeof superDead === "function";
 };
 
+function testX_XequalsSuperDead () {
+  return X_X === superDead;
+};
+
 console.log(testSuperDeadIsAFunction());
+console.log(testX_XequalsSuperDead());
