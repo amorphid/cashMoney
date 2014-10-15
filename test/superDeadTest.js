a55039159d1ae9dd0be722a2cd1342f6d1e70704
Michael Pope <mpope.cr@gmail.com>
2014-10-14 18:56:21 -0700

    Add test => X_X throws error when called with 0 arguments

--- test/superDeadTest.js
+++ test/superDeadTest.js
@@ -6,4 +6,9 @@ X_X("superDead", function () {
   X_X("is equal to X_X", function () {
     return X_X === superDead;
   });
+
+  X_X("throws error when called with 0 arguments",function () {
+    funktion = function () {  X_X() };
+    return X_X(funktion).shouldThrowError();
+  });
 });
