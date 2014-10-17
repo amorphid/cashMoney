a4492f432489e96cf0ce70c624f0cb37489540f8
Michael Pope <mpope.cr@gmail.com>
2014-10-17 15:44:33 -0700

    Break test => X_X.equals is valid with equal inputs

--- test/superDeadTest/equalsTest.js
+++ test/superDeadTest/equalsTest.js
@@ -6,6 +6,6 @@
 
 X_X("X_X.equals", function () {
   X_X("returns true if inputs are equal", function () {
-    return function () { return X_X(123).equals(123) };
+    X_X(123).equals(123);
   });
 });
