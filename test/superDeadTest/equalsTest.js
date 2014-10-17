abccf21de02cca3928363146542538438894d734
Michael Pope <mpope.cr@gmail.com>
2014-10-17 16:27:39 -0700

    Fix X_X.equals is valid with equal inputs

--- test/superDeadTest/equalsTest.js
+++ test/superDeadTest/equalsTest.js
@@ -1,9 +1,3 @@
-// X_X("X_X.equals", function () {
-//   X_X("returns true if inputs are equal", function () {
-//     return X_X(123).equals(123) === true
-//   });
-// });
-
 X_X("X_X.equals", function () {
   X_X("returns true if inputs are equal", function () {
     X_X(123).equals(123);
