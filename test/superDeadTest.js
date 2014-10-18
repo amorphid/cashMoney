38d980f3ea719e653eb6dc667077bae13668ea33
Michael Pope <mpope.cr@gmail.com>
2014-10-17 17:32:36 -0700

    Refactor tests & add test => X_X.equals is a function

--- test/superDeadTest.js
+++ test/superDeadTest.js
@@ -1,19 +1,19 @@
-// X_X("superDead", function () {
-//   X_X("is a function", function () {
-//     return typeof superDead === "function";
-//   });
+X_X("superDead", function () {
+  X_X("is a function", function () {
+    X_X(typeof superDead).equals("function");
+  });
 
-//   X_X("is equal to X_X", function () {
-//     return X_X === superDead;
-//   });
+  X_X("is equal to X_X", function () {
+    X_X(superDead).equals(X_X);
+  });
 
-//   X_X("throws error when called with 0 arguments",function () {
-//     funktion = function () {  X_X() };
-//     return X_X(funktion).shouldThrowError();
-//   });
+  // X_X("throws error when called with 0 arguments",function () {
+  //   funktion = function () {  X_X() };
+  //   return X_X(funktion).shouldThrowError();
+  // });
 
-//   X_X("throws error when called with 3+ arguments",function () {
-//     funktion = function () {  X_X(1, 2, 3) };
-//     return X_X(funktion).shouldThrowError();
-//   });
-// });
+  // X_X("throws error when called with 3+ arguments",function () {
+  //   funktion = function () {  X_X(1, 2, 3) };
+  //   return X_X(funktion).shouldThrowError();
+  // });
+});
