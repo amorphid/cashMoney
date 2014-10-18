cf475329a31f81a7ff77ccaf1a7be9e3904ba348
Michael Pope <mpope.cr@gmail.com>
2014-10-18 15:51:07 -0700

    Add test => X_X.Test.shouldThrowError throws error w/ 1+ arguments

--- test/superDeadTest/Test/shouldThrowErrorTest.js
+++ test/superDeadTest/Test/shouldThrowErrorTest.js
@@ -2,4 +2,12 @@ X_X("X_X.Test.shouldThrowError", function () {
   X_X("is a function", function () {
     X_X(typeof X_X.Test.shouldThrowError).equals("function");
   });
+
+  X_X("throws error when called with 1+ arguments",function () {
+    funktion = function () {
+      X_X(function () {}).shouldThrowError(123)
+    };
+
+    X_X(funktion).shouldThrowError();
+  });
 });
