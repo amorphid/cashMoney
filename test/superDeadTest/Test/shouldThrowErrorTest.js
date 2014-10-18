2fd593c8f1505daf5b78bb86c46f1ea050ea2b9f
Michael Pope <mpope.cr@gmail.com>
2014-10-18 16:05:00 -0700

    X_X.Test.shouldThrowError throws error w/ 1+ arguments

--- test/superDeadTest/Test/shouldThrowErrorTest.js
+++ test/superDeadTest/Test/shouldThrowErrorTest.js
@@ -3,7 +3,7 @@ X_X("X_X.Test.shouldThrowError", function () {
     X_X(typeof X_X.Test.shouldThrowError).equals("function");
   });
 
-  X_X("throws error when called with 1+ arguments",function () {
+  X_X("throws error 1+ inputs",function () {
     funktion = function () {
       X_X(function () {}).shouldThrowError(123)
     };
