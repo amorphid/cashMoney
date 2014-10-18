64e103ca60a2d3efbb5df23b93e19f26467eda54
Michael Pope <mpope.cr@gmail.com>
2014-10-18 15:46:15 -0700

    Move code from X_X.shouldThrowError to X_X.Test.shouldThrowError

---
+++ test/superDeadTest/Test/shouldThrowErrorTest.js
@@ -0,0 +1,5 @@
+X_X("X_X.Test.shouldThrowError", function () {
+  X_X("is a function", function () {
+    X_X(typeof X_X.Test.shouldThrowError).equals("function");
+  });
+});
--- test/superDeadTest/shouldThrowErrorTest.js
+++
@@ -1,5 +0,0 @@
-X_X("X_X.shouldThrowError", function () {
-  X_X("is a function", function () {
-    X_X(typeof X_X.shouldThrowError).equals("function");
-  });
-});
