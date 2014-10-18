ff2bb5ef9807ce44c8d507e75fdfa4fb043a08bd
Michael Pope <mpope.cr@gmail.com>
2014-10-18 15:34:49 -0700

    Add 2 tests => X_X.Test.equals throws errors with <1 or >1 argument

--- test/superDeadTest/Test/equalsTest.js
+++ test/superDeadTest/Test/equalsTest.js
@@ -6,4 +6,14 @@ X_X("X_X.Test.equals", function () {
   X_X("returns true if inputs are equal", function () {
     X_X(123).equals(123);
   });
+
+  X_X("throws error when called with 0 arguments",function () {
+    funktion = function () {  X_X(123).equals() };
+    X_X(funktion).shouldThrowError();
+  });
+
+  X_X("throws error when called with 2+ arguments",function () {
+    funktion = function () {  X_X(123).equals(456, 789) };
+    X_X(funktion).shouldThrowError();
+  });
 });
