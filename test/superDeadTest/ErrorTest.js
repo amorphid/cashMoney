a194607a0f354db4af5814a6e42ae426359f255f
Michael Pope <mpope.cr@gmail.com>
2014-10-18 17:12:13 -0700

    Add test => X_X.Error should throw error

--- test/superDeadTest/ErrorTest.js
+++ test/superDeadTest/ErrorTest.js
@@ -2,4 +2,15 @@ X_X("X_X.Error", function () {
   X_X("is a function", function () {
     X_X(typeof X_X.Error).equals("function");
   });
+
+  X_X("should throw error", function () {
+    var funktion = function () {
+      X_X.Error(
+        "ArgumentError",
+        "Wrong number of arguments (0 for 1)"
+      );
+    };
+
+    X_X(funktion).shouldThrowError();
+  });
 });
