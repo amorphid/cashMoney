0d039660d51af9756c9da8fe97305f30463db3ef
Michael Pope` <amorphid@Michaels-MacBook-Pro.local>
2014-10-26 09:37:34 -0700

    X_X.ArgumentsValidation is a function => true

---
+++ test/superDeadTest/ArgumentsValidationTest.js
@@ -0,0 +1,24 @@
+X_X("X_X.ArgumentsValidation", function () {
+  X_X("is a function", function () {
+    X_X(typeof X_X.ArgumentsValidation).equals("function");
+  });
+
+  // X_X("returns an object", function () {
+  //   var args = {
+  //     args: { a: 1, b: 2 },
+  //     count: 2
+  //   };
+
+  //   X_X(typeof X_X.ArgumentsValidation(args)).equals("object");
+  // });
+
+  // X_X("throws error w/ 0 inputs",function () {
+  //   funktion = function () {  X_X.Test(); };
+  //   X_X(funktion).shouldThrowError();
+  // });
+
+  // X_X("throws error w/ 0 inputs",function () {
+  //   funktion = function () {  X_X.Test(); };
+  //   X_X(funktion).shouldThrowError();
+  // });
+});
